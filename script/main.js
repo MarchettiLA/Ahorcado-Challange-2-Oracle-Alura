ad6bb3e9660bafae2b9f437dc8886ffb946bff33
Lautaro <lautaroamarchetti@gmail.com>
2022-06-03 17:43:51 -0300

    Terminada pantalla inicial

--- script/main.js
+++ script/main.js
@@ -10,21 +10,31 @@ const moverCalavera = () => {
     window.onload = moverCalavera
 
 // FUNCION QUE MUESTRA O ESCONDE EL DIALOGO ON-CLICK
-
+const dialogosCalavera = ["Bienvenido... Contendiente.", "Elije una opción... No tengo todo el día.", "Ya deja de molestarme!!!", "..."];
+let dialogIndex = 0;
 let visible = false
 
         calavera.addEventListener("click", (event) => {
+            dialog.textContent = dialogosCalavera[dialogIndex];
             
+            if(dialogIndex >= dialogosCalavera.length){
+                dialogIndex = 3;
+            }
             if(visible == false){
                 dialog.classList.add("visibleDialog")
                 visible = true;
                                 
             } else {
-                console.log("hola")
                 dialog.classList.replace("visibleDialog", "hideDialog")
-                visible = false
+                visible = false;
+                if(dialogIndex == dialogosCalavera.length - 1){
+                    dialogIndex = 3
+                } else{
+                    dialogIndex += 1;  
+                }
             }
-        });
+        })
+    
        
       
     
